7535abd25af387c8eeb1eb59eae6651e45a86b85
artyomdev250 <artyom.dev250@gmail.com>
2025-12-05 21:54:23 +0500

    edit product setup

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const upload = require('../config/multer');
 const createProduct = require('../controllers/createProductController');
-const { getProducts } = require('../controllers/productController');
+const updateProduct = require('../controllers/updateProductController');
 const deleteProduct = require('../controllers/deleteProductController');
+const { getProducts } = require('../controllers/productController');
 
 router.get('/products', getProducts);
 router.post('/products', upload.single('image'), createProduct);
+router.put('/products/:id', upload.single('image'), updateProduct);
 router.delete('/products/:id', deleteProduct);
 
 module.exports = router;
